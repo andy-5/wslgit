3254818133637a7d2d15f429cbcb7af78f8140c6
Carl-Oskar Larsson <carloskar.larsson@gmail.com>
2020-01-13 22:14:50 +0100

    Added script for creating a release zip file and an install script that should be run after unzipping, and updated installation instructions.

---
+++ create-release-package.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+WSLGIT_BINARY=target/release/wslgit.exe
+OUTPUT_DIR=release/wslgit
+
+[[ ! -f "$WSLGIT_BINARY" ]] && echo "Release not built!" && exit 1
+
+rm -rf release/* || exit 1
+mkdir -p $OUTPUT_DIR/bin || exit 1
+
+cp "$WSLGIT_BINARY" "$OUTPUT_DIR/bin" || exit 1
+cp resources/install.bat "$OUTPUT_DIR" || exit 1
+
+cd release && zip -r wslgit.zip ./*
---
+++ resources/install.bat
@@ -0,0 +1,62 @@
+@echo off
+net session >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+   echo.
+   echo This script must be run as administrator to work properly!
+   echo Right click on the script and select "Run as administrator".
+   echo.
+   goto :error
+)
+
+set CWD=%~dp0
+set BINDIR=%CWD%bin
+cd %CWD%
+
+echo.
+if exist "%BINDIR%\git.exe" (
+    echo 'git.exe' already exist.
+) else (
+    echo Create 'git.exe' symlink...
+    mklink %BINDIR%\git.exe %BINDIR%\wslgit.exe
+    if %ERRORLEVEL% neq 0 (
+        echo ERROR! Failed to create symlink.
+        goto :error
+    )
+)
+
+echo.
+if exist "%BINDIR%\sh.exe" (
+    echo 'sh.exe' already exist.
+) else (
+    echo Create 'sh.exe' symlink...
+    mklink %BINDIR%\sh.exe C:\Windows\System32\bash.exe
+    if %ERRORLEVEL% neq 0 (
+        echo ERROR! Failed to create symlink.
+        goto :error
+    )
+)
+
+echo.
+if exist "%BINDIR%\bash.exe" (
+    echo 'bash.exe' already exist.
+) else (
+    echo Create 'bash.exe' symlink...
+    mklink %BINDIR%\bash.exe C:\Windows\System32\bash.exe
+    if %ERRORLEVEL% neq 0 (
+        echo ERROR! Failed to create symlink.
+        goto :error
+    )
+)
+
+echo.
+echo Installation successful!
+echo.
+echo Add to the Windows Path environment variable (user or system) to use as system git:
+echo  %BINDIR%
+echo.
+pause
+exit /B 0
+
+:error
+pause
+exit /B 1
