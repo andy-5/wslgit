1ca42542ba22ea8f0b497973ed11f40a6d0c5362
Carl-Oskar Larsson <carloskar.larsson@gmail.com>
2020-02-09 12:48:17 +0100

    Changed directory structure to bin and cmd.

--- create-release-package.sh
+++ create-release-package.sh
@@ -2,14 +2,15 @@
 
 WSLGIT_BINARY=target/release/wslgit.exe
 OUTPUT_DIR=release/wslgit
+OUTPUT_CMD_DIR=$OUTPUT_DIR/cmd
 
 [[ ! -f "$WSLGIT_BINARY" ]] && echo "Release not built!" && exit 1
 
 rm -rf release/* || exit 1
-mkdir -p $OUTPUT_DIR/bin || exit 1
+mkdir -p $OUTPUT_CMD_DIR || exit 1
 
-cp "$WSLGIT_BINARY" "$OUTPUT_DIR/bin" || exit 1
-cp resources/Fork.RI "$OUTPUT_DIR/bin" || exit 1
+cp "$WSLGIT_BINARY" "$OUTPUT_CMD_DIR" || exit 1
+cp resources/Fork.RI "$OUTPUT_CMD_DIR" || exit 1
 cp resources/install.bat "$OUTPUT_DIR" || exit 1
 
 cd release && zip -r wslgit.zip ./*
